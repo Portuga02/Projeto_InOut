d9d7c3844d0bf0705a005ea31a660fbc6b0733df
savio <savio@192.168.1.5>
2020-08-07 17:37:52 -0300

    configurando padroes de rotas dentro do config.php

---
+++ src/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// configurações e locais de tempo e idioma
+date_default_timezone_set('America/Sao_Paulo');
+setlocale(LC_TIME, 'pt-br', 'pt_BR.utf-8', 'portuguese');
+
+define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
+
+
+require_once(realpath(dirname(__FILE__) . '/database.php'));
